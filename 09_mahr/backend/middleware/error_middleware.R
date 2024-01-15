e0478e7769d6cb8b9f2e16e235b38cf4ae3509f9
kennedymwavu <mwavukennedy@gmail.com>
2024-01-15 18:28:31 +0300

    ft: update error handler to take in the error

--- 09_mahr/backend/middleware/error_middleware.R
+++ 09_mahr/backend/middleware/error_middleware.R
@@ -1,11 +1,16 @@
 box::use(
+  cli[cli_alert_danger],
   .. / helpers / operators[`%||%`]
 )
 
 #' Error handler
 #'
 #' @export
-error_handler <- \(req, res) {
+error_handler <- \(req, res, error = NULL) {
+  if (!is.null(error)) {
+    error_msg <- conditionMessage(error)
+    cli_alert_danger("Error: {error_msg}")
+  }
   response <- list(msg = "A server error occurred!")
   res$
     set_status(500L)$
