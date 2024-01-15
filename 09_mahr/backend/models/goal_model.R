4836bf38ac3f3058576f192ad23ec437113ee7ea
kennedymwavu <mwavukennedy@gmail.com>
2024-01-15 23:03:21 +0300

    ch: add model schemas for reference

---
+++ 09_mahr/backend/models/goal_model.R
@@ -0,0 +1,6 @@
+#' Goal schema
+#' 
+goal_schema <- data.frame(
+  user = character(), # oid
+  text = character()
+)
---
+++ 09_mahr/backend/models/user_model.R
@@ -0,0 +1,8 @@
+#' User schema
+#' 
+user_schema <- data.frame(
+  name = character(),
+  email = character(),
+  password = character(),
+  created_at = character() # timestamp
+)
