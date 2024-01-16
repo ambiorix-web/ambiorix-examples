c081fc4584997c9cd355d9e90d7d6afa9e77b093
kennedymwavu <mwavukennedy@gmail.com>
2024-01-16 19:20:27 +0300

    ft: define users db connection

--- 09_mahr/backend/config/db.R
+++ 09_mahr/backend/config/db.R
@@ -7,3 +7,8 @@ box::use(
 #' 
 #' @export 
 goals_conn <- mongo(collection = "goals", db = "mahr_tutorial")
+
+#' Users db connection
+#' 
+#' @export 
+users_conn <- mongo(collection = "users", db = "mahr_tutorial")
