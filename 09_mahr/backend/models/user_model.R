e34a44e6e471a6065d1b00cbc286008eca4ccbaf
kennedymwavu <mwavukennedy@gmail.com>
2024-01-16 19:22:47 +0300

    ch: update user schema to accept an 'updated_at' field

--- 09_mahr/backend/models/user_model.R
+++ 09_mahr/backend/models/user_model.R
@@ -4,5 +4,6 @@ user_schema <- data.frame(
   name = character(),
   email = character(),
   password = character(),
-  created_at = character() # timestamp
+  created_at = character(), # timestamp: UTC datetime format
+  updated_at = character() # timestamp: UTC datetime format
 )
