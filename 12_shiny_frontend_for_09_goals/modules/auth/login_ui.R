9f79fae50a69709fcad61b5970318cdee3ca76d7
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-07-24 21:43:57 +0300

    ft: create auth page

--- 12_shiny_frontend_for_09_goals/modules/auth/login_ui.R
+++ 12_shiny_frontend_for_09_goals/modules/auth/login_ui.R
@@ -1,53 +1,7 @@
 box::use(
-  htmltools[tags],
-  shiny[
-    NS,
-    actionLink,
-    actionButton
-  ],
-  .. / .. / store / mod[
-    email_input,
-    password_input,
-  ],
-  . / signup_ui[
-    create_auth_form_header,
-    create_auth_form_footer,
-  ],
+  shiny[NS],
+  . / signup_ui[create_auth_page],
 )
-
-#' Sign up page
-#'
-#' @param ns Namespace of the module from which this function is called.
-#' @return [htmltools::tags()]
-#' @export
-page <- \(ns) {
-  tags$div(
-    class = "container vh-100 d-flex justify-content-center align-items-center",
-    tags$div(
-      tags$form(
-        create_auth_form_header(type = "login"),
-        email_input(
-          inputId = ns("email"),
-          label = "Email"
-        ),
-        password_input(
-          ns = ns,
-          input_id = "password",
-          label = "Password"
-        ),
-        tags$div(
-          class = "d-grid",
-          actionButton(
-            inputId = ns("login"),
-            label = "Login",
-            type = "submit"
-          )
-        ),
-        create_auth_form_footer(ns = ns, type = "login")
-      )
-    )
-  )
-}
 
 #' Login UI module
 #'
@@ -55,5 +9,5 @@ page <- \(ns) {
 #' @export
 ui <- \(id) {
   ns <- NS(id)
-  page(ns)
+  create_auth_page(ns = ns, type = "login")
 }
--- 12_shiny_frontend_for_09_goals/modules/auth/signup_ui.R
+++ 12_shiny_frontend_for_09_goals/modules/auth/signup_ui.R
@@ -68,40 +68,84 @@ create_auth_form_footer <- \(
   )
 }
 
-#' Sign up page
+#' Create auth form submit button
+#'
+#' @param ns Module namespace from which this function is called.
+#' @param type String. Type of form. Either "signup" (default) or "login".
+#' @return [htmltools::tags$div()]
+#' @export
+create_auth_form_btn <- \(
+  ns,
+  type = c("signup", "login")
+) {
+  type <- match.arg(arg = type)
+
+  input_id <- ns("signup")
+  label <- "Create Account"
+
+  is_login <- identical(type, "login")
+  if (is_login) {
+    input_id <- ns("login")
+    label <- "Login"
+  }
+
+  tags$div(
+    class = "d-grid",
+    actionButton(
+      inputId = input_id,
+      label = label,
+      type = "submit"
+    )
+  )
+}
+
+#' Auth form
+#'
+#' @param ns Module namespace from which this function is called.
+#' @param type String. Type of form. Either "signup" (default) or "login".
+#' @return [htmltools::tags$form()]
+#' @export
+auth_form <- \(
+  ns,
+  type = c("signup", "login")
+) {
+  type <- match.arg(arg = type)
+  is_signup <- identical(type, "signup")
+
+  tags$form(
+    create_auth_form_header(type = type),
+    if (is_signup) {
+      text_input(
+        inputId = ns("name"),
+        label = "First & Last Name"
+      )
+    },
+    email_input(
+      inputId = ns("email"),
+      label = "Email"
+    ),
+    password_input(
+      ns = ns,
+      input_id = "password",
+      label = "Password"
+    ),
+    create_auth_form_btn(ns = ns, type = type),
+    create_auth_form_footer(ns = ns, type = type)
+  )
+}
+
+#' Create auth page
 #'
 #' @param ns Namespace of the module from which this function is called.
 #' @return [htmltools::tags$div()]
 #' @export
-page <- \(ns) {
+create_auth_page <- \(ns, type = c("signup", "login")) {
+  type <- match.arg(arg = type)
+
   tags$div(
     class = "container vh-100 d-flex justify-content-center align-items-center",
     tags$div(
-      tags$form(
-        create_auth_form_header(type = "signup"),
-        text_input(
-          inputId = ns("name"),
-          label = "First & Last Name"
-        ),
-        email_input(
-          inputId = ns("email"),
-          label = "Email"
-        ),
-        password_input(
-          ns = ns,
-          input_id = "password",
-          label = "Password"
-        ),
-        tags$div(
-          class = "d-grid",
-          actionButton(
-            inputId = ns("signup"),
-            label = "Create Account",
-            type = "submit"
-          )
-        ),
-        create_auth_form_footer(ns = ns, type = "signup")
-      )
+      auth_form(ns = ns, type = type)
     )
   )
 }
@@ -112,5 +156,5 @@ page <- \(ns) {
 #' @export
 ui <- \(id) {
   ns <- NS(id)
-  page(ns)
+  create_auth_page(ns = ns, type = "signup")
 }
