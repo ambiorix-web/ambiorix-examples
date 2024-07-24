0b78c4f08aff919ba7bd67fc0a2702e7d78007e9
Kennedy Mwavu <mwavukennedy@gmail.com>
2024-07-25 02:25:14 +0300

    ft: add proxies to update & delete account

--- 12_shiny_frontend_for_09_goals/modules/auth/proxy.R
+++ 12_shiny_frontend_for_09_goals/modules/auth/proxy.R
@@ -55,13 +55,56 @@ login <- \(email, password) {
 
 #' Get user account details
 #'
-#' @param token JWT token
+#' @param token String. JWT token.
 #' @return Named list.
 #' @export
 get_account_details <- \(token) {
   request(base_url = get_base_url()) |>
     req_url_path("/api/users/me") |>
     req_auth_bearer_token(token = token) |>
+    req_perform() |>
+    resp_body_json()
+}
+
+#' Update user account details
+#'
+#' @param name String. Name of user.
+#' @param email String. User email.
+#' @param password String. Password.
+#' @param token String. JWT token.
+#' @return Named list.
+#' @export
+update_account_details <- \(
+  name = NULL,
+  email = NULL,
+  password = NULL,
+  token
+) {
+  new_details <- list(
+    name = name,
+    email = email,
+    password = password
+  )
+
+  request(base_url = get_base_url()) |>
+    req_url_path("/api/users/me") |>
+    req_auth_bearer_token(token = token) |>
+    req_method(method = "PUT") |>
+    req_body_multipart(!!!new_details) |>
+    req_perform() |>
+    resp_body_json()
+}
+
+#' Delete user account
+#'
+#' @param token String. JWT token.
+#' @return Named list.
+#' @export
+delete_account <- \(token) {
+  request(base_url = get_base_url()) |>
+    req_url_path("/api/users/me") |>
+    req_auth_bearer_token(token = token) |>
+    req_method(method = "DELETE") |>
     req_perform() |>
     resp_body_json()
 }
